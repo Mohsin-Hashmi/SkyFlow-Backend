2e3d9587641dc70d6284eb7e280605385f6bc573
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2026-04-22 04:34:05 +0500

    feat:/implement the zod schema for users and start working on figlt module

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { register, login, logout, getProfile } from '../../services/auth/auth.service';
 import { authenticate } from '../../middleware/auth.middleware';
+import { validate } from '../../middleware/validate.middleware';
+import { createUserSchema, loginUserSchema } from '../../schema/user.validation';
 
 export const authRouter = express.Router();
 
 
-authRouter.post('/register', register)
-authRouter.post('/login', login)
+authRouter.post('/register', validate(createUserSchema), register)
+authRouter.post('/login', validate(loginUserSchema), login)
 authRouter.post('/logout', logout)
 authRouter.get('/profile', authenticate, getProfile)
--- src/routes/flight/flight.routes.ts
+++ src/routes/flight/flight.routes.ts
@@ -3,4 +3,4 @@ export const flightRouter = express.Router();
 import { createFlight } from '../../controllers/flight/flight.controller';
 import { authenticate } from '../../middleware/auth.middleware';
 
-flightRouter.post('/create-flight', authenticate, createFlight);
+flightRouter.post('/create-flight/:airlineId', authenticate, createFlight);
